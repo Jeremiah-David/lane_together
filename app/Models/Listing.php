b93eee295a1ce16a081687772fbb0806951f38ef
Jeremiah J David <Dancesfire@gmail.com>
2024-01-20 15:56:05 -0800

    made laravel model listing

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,35 +1,11 @@
-<?php 
-    namespace App\Models;
-    use Symfony\Component\Console\SingleCommandApplication;
+<?php
 
-    class Listing {
-        //  All listing
-        public static function all() {
-            return [
-                [    'id'    => 1,
-                    'title' => 'Listing One',
-                    'description' => 'This is a description of Listing One'
-                ],
-                [    'id'    => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'This is a description of Listing One'
-                ],  
-                [   
-                     'id'    => 3,
-                    'title' => 'Listing Three',
-                    'description' => 'This is a description of Listing One'
-                ]
-            ];
-        }
+namespace App\Models;
 
-// Single listing
-        public static function find($id) {
-            $listings = self::all();
-            foreach($listings as $listing) {
-                if($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-            
-        }
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
